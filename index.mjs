38b03fe9f2c5596f1c806ff990e0247979de262d
Michael McNeil <me@michaelmcneil.net>
2019-12-09 15:26:49 -0500

    Updated for es6

--- index.mjs
+++ index.mjs
@@ -1,10 +1,54 @@
-const { Time } = require("./modules/duration/index.js/index.js.js");
-const { Distance } = require("./modules/distance/index.js/index.js");
-const { Interval } = require("./modules/interval.js");
+/**
+ * @module dfit
+ */
 
-export const distance = (value, unit) => Distance.of(value, unit);
+const Duration = require("./modules/duration");
+const Distance = require("./modules/distance");
+const Interval = require("./modules/interval");
+const Speed = require("./modules/speed");
+const Pace = require("./modules/pace");
 
-export const time = (value, format) => Time.of(value, format);
+/**
+ * Creates a Duration object (wraps new Duration)
+ * @param  {...(Object|Number|String)} args
+ * - {Object} with optional keys ms|s|m|h|d|y
+ * - {Number} duration, {String} [unit=DEFAULT_DURATION_UNIT]
+ * @returns Duration
+ */
+export const duration = (...args) => new Duration(...args);
 
-export const interval = (distance, distanceUnit, time, timeFormat) =>
-  Interval.of(distance, distanceUnit, time, timeFormat);
+/**
+ * Creates a Distance object (wraps new Distance)
+ * @param  {...(Object|Number|String)} args
+ * - {Object} with optional keys mm|cm|m|km|mile|miles
+ * - {Number} distance, {String} [unit=DEFAULT_DISTANCE_UNIT]
+ * @returns Distance
+ */
+export const distance = (...args) => new Distance(...args);
+
+/**
+ * Creates and Interval object (wraps new Interval)
+ * @param  {...(Duration|Distance|Pace|Speed)} args - Any order of Duration, Distance, Pace, or Speed
+ * @returns Interval
+ */
+export const interval = (...args) => new Interval(...args);
+
+/**
+ * Creates a Speed object (wraps new Speed)
+ * @param  {...(Duration|Distance|Number)} args
+ * - {Duration}, {Distance}
+ * - {Distance}, {Duration}
+ * - {Number} as mph
+ * - ... otherwise defaults to 0 mph
+ * @returns Speed
+ */
+export const speed = (...args) => new Speed(...args);
+
+/**
+ * Creates Pace object (wraps new Pace)
+ * @param {...(Duration|Distance|object|number|string)} args
+ * - {Duration, {Distance}
+ * - {Number|Object} duration, {Number|Object} distance, {String} [durationUnit], {String} [distanceUnit]
+ * @returns Pace
+ */
+export const pace = (...args) => new Pace(...args);
